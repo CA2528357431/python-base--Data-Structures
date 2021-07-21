6cf9907f39cc3740935e9dd2fcd6598e245cffec
CA2528357431 <2528357431@QQ.com>
2021-07-22 00:55:28 +0800

    normal

--- 09/main.py
+++ 09/main.py
@@ -50,6 +50,8 @@ def swap(tar):
                 cur = sta.pop()
                 res += " " + cur
                 sta.push(x)
+            else:
+                sta.push(x)
         elif x in "*/":
             sta.push(x)
         else:
@@ -61,7 +63,7 @@ def swap(tar):
     return res
 
 
-tar = "a + ( b + c ) * d - e"
+tar = "( a + ( b + c ) * d ) - e"
 res = swap(tar).strip()
 print(res)
 
@@ -79,7 +81,7 @@ print(res)
 如果不满足上述三个条件，那么栈的状态必然是下面这种可能：
 栈不空，栈顶元素不是(，那么栈顶元素是个操作符，且优先级比当前元素高或相等，说明栈顶的这个操作符，是时候完成计算了，不需要再等当前元素了，所以栈顶元素应该出栈。之后，再继续判断栈的状态是否满足4.1、4.2、4.3这三个条件中的一个。
 ————————————————
-版权声明：本文为CSDN博主「彭于晏湖北分晏」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+
 原文链接：https://blog.csdn.net/ten_sory/article/details/109624798
 '''
 
--- 22/main.py
+++ 22/main.py
@@ -1,16 +1,165 @@
-# This is a sample Python script.
+class tree:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, root, left=None, right=None):
+        self.nodes = []
+        self.root = root
+        self.left = left
+        self.right = right
+
+    @property
+    def lisp(self):
+        lisp = [self.root, None, None]
+        if self.left is not None:
+            lisp[1] = self.left.lisp
+        if self.right is not None:
+            lisp[2] = self.right.lisp
+        return lisp
+        # lisp 表达法
+
+    def __str__(self):
+        return str(self.lisp)
+
+    def first(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.first()
+        if self.right is not None:
+            r = self.right.first()
+        res = [self.root] + l + r
+
+        return res
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    def middle(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.middle()
+        if self.right is not None:
+            r = self.right.middle()
+        res = l + [self.root] + r
+
+        return res
+
+    def last(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.last()
+        if self.right is not None:
+            r = self.right.last()
+        res =  l + r + [self.root]
+
+        return res
+
+    # 一种广度优先遍历
+
+    def layer(self):
+        res = []
+        queue = [self]
+        # queue中同层的数据相连
+        while queue:
+            cur = queue[0]
+            queue.pop(0)
+            res.append(cur.root)
+            for x in (cur.left,cur.right):
+                if x is not None:
+                    queue.append(x)
+        return res
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class stack:
+    def __init__(self):
+        self.data = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def push(self, x):
+        self.data.append(x)
+
+    def pop(self):
+        if self.empty():
+            return None
+        else:
+            x = self.data[len(self.data) - 1]
+            self.data.pop()
+            return x
+
+    def top(self):
+        if self.empty():
+            return None
+        else:
+            return self.data[len(self.data) - 1]
+
+    def empty(self):
+        if self.data:
+            return False
+        else:
+            return True
+
+
+def swap(tar):
+    sta = stack()
+    data = tar.split(" ")
+    res = []
+    for x in data:
+        if x in "()" :
+            if x == "(":
+                sta.push(x)
+            else:
+                while sta.top() != "(":
+
+                    l = len(res)
+                    t1 = res[l - 1]
+                    t2 = res[l - 2]
+                    res.pop()
+                    res.pop()
+
+                    t3 = tree(sta.top(), t2, t1)
+                    res.append(t3)
+
+                    sta.pop()
+
+                sta.pop()
+        elif x in "+-":
+            if sta.empty():
+                sta.push(x)
+            elif sta.top() in "*/":
+                cur = sta.pop()
+
+                l = len(res)
+                t1 = res[l - 1]
+                t2 = res[l - 2]
+                res.pop()
+                res.pop()
+
+                t3 = tree(cur, t2, t1)
+                res.append(t3)
+
+                sta.push(x)
+            else:
+                sta.push(x)
+        elif x in "*/":
+            sta.push(x)
+        else:
+            t = tree(x)
+            res.append(t)
+
+    while not sta.empty():
+        cur = sta.pop()
+
+        l = len(res)
+        t1 = res[l - 1]
+        t2 = res[l - 2]
+        res.pop()
+        res.pop()
+
+        t3 = tree(cur, t2, t1)
+        res.append(t3)
+
+    return res[0]
+
+
+tar = "( a + ( b + c ) * d ) - e"
+res = swap(tar)
+print(res)
