1b83baec8a34df01856c0ac3f99c74a5907d98f9
CA2528357431 <2528357431@QQ.com>
2021-07-21 23:55:17 +0800

    normal

--- 21/21.py
+++ 21/21.py
@@ -1,1 +1,92 @@
-# 树
+# 二叉树
+
+
+class tree:
+
+    def __init__(self, root, left=None, right=None):
+        self.nodes = []
+        self.root = root
+        self.left = left
+        self.right = right
+
+    @property
+    def lisp(self):
+        lisp = [self.root, None, None]
+        if self.left is not None:
+            lisp[1] = self.left.lisp
+        if self.right is not None:
+            lisp[2] = self.right.lisp
+        return lisp
+        # lisp 表达法
+
+    def __str__(self):
+        return str(self.lisp)
+
+    # 三种深度优先遍历
+    # 即三种周游
+    # 周游一定是 根、左周游、右周游的组合
+
+    def first(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.first()
+        if self.right is not None:
+            r = self.right.first()
+        res = [self.root] + l + r
+
+        return res
+
+
+
+    def middle(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.middle()
+        if self.right is not None:
+            r = self.right.middle()
+        res = l + [self.root] + r
+
+        return res
+
+    def last(self):
+        l = []
+        r = []
+        if self.left is not None:
+            l = self.left.last()
+        if self.right is not None:
+            r = self.right.last()
+        res =  l + r + [self.root]
+
+        return res
+
+    # 一种广度优先遍历
+
+    def layer(self):
+        res = []
+        queue = [self]
+        # queue中同层的数据相连
+        while queue:
+            cur = queue[0]
+            queue.pop(0)
+            res.append(cur.root)
+            for x in (cur.left,cur.right):
+                if x is not None:
+                    queue.append(x)
+        return res
+
+
+
+a = tree(1)
+b = tree(2)
+c = tree(3, a, b)
+d = tree(6)
+e = tree(4)
+f = tree(10, d, e)
+g = tree(13, c, f)
+
+print(g.first())
+print(g.middle())
+print(g.last())
+print(g.layer())
